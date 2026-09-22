9d1a5cb4e1363ad958a971ddcd6a8686ee6e42ff
jishan khan <thejishangaddi2004@gmail.com>
2026-09-22 17:03:12 +0530

    Update grafana-promethious-final.sh

--- grafana-promethious-final.sh
+++ grafana-promethious-final.sh
@@ -128,7 +128,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "e35f6dbcd8354200d07090c1855434fc"
+      - routing_key: "86b050c6e4064b04c0c43bb8934ea974"
         severity: "critical"
 EOF
 
